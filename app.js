5e8e44c30176f846125866896b8fb399011c8297
aditiaprasdev <aditiapras.dev@gmail.com>
2023-07-24 14:46:17 +0700

    Unofficial Fotmob API

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+var moment = require("moment-timezone");
+const apiKeys = process.env.MY_API_KEY;
+
+require("dotenv").config();
+
+app.use(cors());
+
+// API HOMEPAGE
+app.get("/", (req, res) => {
+  res.send("Welcome to Unofficial Fotmob Football API");
+});
+
+// GET REQUEST MATCHES
+app.get(
+  "/v1/matches",
+  (req, res, next) => {
+    if (req.query.apikey == apiKeys) next("route");
+    else next();
+  },
+  (req, res, next) => {
+    // send a regular response
+    res.json({ Error, message: "Your are not authorized to access this API." });
+    console.log(req.query);
+    console.log(apiKeys);
+  }
+);
+
+app.get(`/v1/matches`, (req, res, next) => {
+  const date = req.query.date;
+  const timezone = req.query.timezone;
+  const ccode = req.query.ccode3;
+  console.log(date);
+
+  const getData = async () => {
+    const data = await fetch(
+      `https://www.fotmob.com/api/matches?date=${date}&timezone=${timezone}&ccode3=${ccode}`
+    ).then((r) => r.json());
+
+    const result = await data;
+
+    res.json({
+      date: date,
+      timezone: timezone,
+      countryCode: ccode,
+      responses: result,
+    });
+  };
+  getData();
+});
+//
+
+// GET REQUEST ALL LEAGUES
+app.get(
+  "/v1/allLeagues",
+  (req, res, next) => {
+    // if the user ID is 0, skip to the next route
+    if (req.query.apikey == apiKeys) next("route");
+    // otherwise pass the control to the next middleware function in this stack
+    else next();
+  },
+  (req, res, next) => {
+    // send a regular response
+    res.json({ Error, message: "Your are not authorized to access this API." });
+    console.log(req.query);
+    console.log(apiKeys);
+  }
+);
+
+app.get(`/v1/allLeagues`, (req, res, next) => {
+  const getData = async () => {
+    const data = await fetch(`https://www.fotmob.com/api/allLeagues`).then(
+      (r) => r.json()
+    );
+    const result = await data;
+    res.json({
+      responses: result,
+    });
+  };
+  getData();
+});
+//
+
+// GET REQUEST LEAGUE DETAILS
+// https://www.fotmob.com/api/leagues?id=47&ccode3=IDN&season=2022/2023
+
+// GET REQUEST TABLES
+// https://www.fotmob.com/api/tltable?leagueId=47
+
+// GET REQUEST MATCH by id
+//www.fotmob.com/api/match?id=4173719
+
+// GET REQUEST MATCH DETAILS by match id
+// https://www.fotmob.com/api/matchDetails?matchId=4183889
+
+// GET REQUEST TEAM DETAILS
+// https://www.fotmob.com/api/teams?id=9825&ccode3=IDN
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
