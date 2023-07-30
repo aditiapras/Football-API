e91630bb6e5a1180823e54c23b3438be30263525
aditiaprasdev <aditiapras97@gmail.com>
2023-07-30 13:11:27 +0700

    Update leagueTables.js

--- leagues/leagueTables.js
+++ leagues/leagueTables.js
@@ -9,74 +9,77 @@ module.exports = (req, res) => {
       const name = tab.data.composite;
       const title = name == true ? "tables" : "table";
       const league = tab.data;
+
       const run = () => {
         const cups = tab.data[title].map((cup) => {
           return {
             groupName: cup.leagueName,
-            table: {
-              all: cup.table.all.map((al) => {
-                return {
-                  name: al.name,
-                  shortName: al.shortName,
-                  id: al.id,
-                  logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
-                  deduction: al.deduction,
-                  ongoing: al.ongoing,
-                  played: al.played,
-                  wins: al.wins,
-                  draws: al.draws,
-                  losses: al.losses,
-                  scoresStr: al.scoresStr,
-                  goalConDiff: al.goalConDiff,
-                  pts: al.pts,
-                  idx: al.idx,
-                  qualColor: al.qualColor,
-                };
-              }),
-              home: cup.table.home.map((al) => {
-                return {
-                  name: al.name,
-                  shortName: al.shortName,
-                  id: al.id,
-                  logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
-                  deduction: al.deduction,
-                  ongoing: al.ongoing,
-                  played: al.played,
-                  wins: al.wins,
-                  draws: al.draws,
-                  losses: al.losses,
-                  scoresStr: al.scoresStr,
-                  goalConDiff: al.goalConDiff,
-                  pts: al.pts,
-                  idx: al.idx,
-                  qualColor: al.qualColor,
-                };
-              }),
-              away: cup.table.away.map((al) => {
-                return {
-                  name: al.name,
-                  shortName: al.shortName,
-                  id: al.id,
-                  logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
-                  deduction: al.deduction,
-                  ongoing: al.ongoing,
-                  played: al.played,
-                  wins: al.wins,
-                  draws: al.draws,
-                  losses: al.losses,
-                  scoresStr: al.scoresStr,
-                  goalConDiff: al.goalConDiff,
-                  pts: al.pts,
-                  idx: al.idx,
-                  qualColor: al.qualColor,
-                };
-              }),
-            },
+
+            all: cup.table.all.map((al) => {
+              return {
+                name: al.name,
+                shortName: al.shortName,
+                id: al.id,
+                logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
+                deduction: al.deduction,
+                ongoing: al.ongoing,
+                played: al.played,
+                wins: al.wins,
+                draws: al.draws,
+                losses: al.losses,
+                scoresStr: al.scoresStr,
+                goalConDiff: al.goalConDiff,
+                pts: al.pts,
+                idx: al.idx,
+                qualColor: al.qualColor,
+              };
+            }),
+            home: cup.table.home.map((al) => {
+              return {
+                name: al.name,
+                shortName: al.shortName,
+                id: al.id,
+                logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
+                deduction: al.deduction,
+                ongoing: al.ongoing,
+                played: al.played,
+                wins: al.wins,
+                draws: al.draws,
+                losses: al.losses,
+                scoresStr: al.scoresStr,
+                goalConDiff: al.goalConDiff,
+                pts: al.pts,
+                idx: al.idx,
+                qualColor: al.qualColor,
+              };
+            }),
+            away: cup.table.away.map((al) => {
+              return {
+                name: al.name,
+                shortName: al.shortName,
+                id: al.id,
+                logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
+                deduction: al.deduction,
+                ongoing: al.ongoing,
+                played: al.played,
+                wins: al.wins,
+                draws: al.draws,
+                losses: al.losses,
+                scoresStr: al.scoresStr,
+                goalConDiff: al.goalConDiff,
+                pts: al.pts,
+                idx: al.idx,
+                qualColor: al.qualColor,
+              };
+            }),
           };
         });
+
         const flow = title == "tables" ? cups : league;
+
         return flow;
       };
+
       const res =
         title == "tables"
           ? {
@@ -102,10 +105,73 @@ module.exports = (req, res) => {
                   light: `https://images.fotmob.com/image_resources/logo/leaguelogo/${tab.data.leagueId}.png`,
                   dark: `https://images.fotmob.com/image_resources/logo/leaguelogo/dark/${tab.data.leagueId}.png`,
                 },
+                tables: [
+                  {
+                    all: tab.data.table.all.map((al) => {
+                      return {
+                        name: al.name,
+                        shortName: al.shortName,
+                        id: al.id,
+                        logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
+                        deduction: al.deduction,
+                        ongoing: al.ongoing,
+                        played: al.played,
+                        wins: al.wins,
+                        draws: al.draws,
+                        losses: al.losses,
+                        scoresStr: al.scoresStr,
+                        goalConDiff: al.goalConDiff,
+                        pts: al.pts,
+                        idx: al.idx,
+                        qualColor: al.qualColor,
+                      };
+                    }),
+                    home: tab.data.table.home.map((al) => {
+                      return {
+                        name: al.name,
+                        shortName: al.shortName,
+                        id: al.id,
+                        logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
+                        deduction: al.deduction,
+                        ongoing: al.ongoing,
+                        played: al.played,
+                        wins: al.wins,
+                        draws: al.draws,
+                        losses: al.losses,
+                        scoresStr: al.scoresStr,
+                        goalConDiff: al.goalConDiff,
+                        pts: al.pts,
+                        idx: al.idx,
+                        qualColor: al.qualColor,
+                      };
+                    }),
+                    away: tab.data.table.away.map((al) => {
+                      return {
+                        name: al.name,
+                        shortName: al.shortName,
+                        id: al.id,
+                        logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${al.id}_xsmall.png`,
+                        deduction: al.deduction,
+                        ongoing: al.ongoing,
+                        played: al.played,
+                        wins: al.wins,
+                        draws: al.draws,
+                        losses: al.losses,
+                        scoresStr: al.scoresStr,
+                        goalConDiff: al.goalConDiff,
+                        pts: al.pts,
+                        idx: al.idx,
+                        qualColor: al.qualColor,
+                      };
+                    }),
+                  },
+                ],
               },
             };
+
       return res;
     });
+
     res.json({ table });
   };
 
