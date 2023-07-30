18ffa3c25472bd49f5d4e7fb42b2e44a97d5cd96
aditiaprasdev <aditiapras97@gmail.com>
2023-07-30 13:11:41 +0700

    Update teamsSquads.js

--- teams/teamsSquads.js
+++ teams/teamsSquads.js
@@ -4,12 +4,47 @@ module.exports = (req, res) => {
     if (!id) {
       return res.json({ result: null, error: "Required Team ID!" });
     }
-
     const squads = await fetch(
       `https://www.fotmob.com/api/teams?id=${id}`
     ).then((r) => r.json());
 
-    res.json({ squad: squads.squad });
+    const returned = () => {
+      const status = squads.squad;
+      const flow = () => {
+        const result = squads.squad.map((squad) => {
+          return {
+            role: squad[0],
+            lists: squad[1].map((list) => {
+              return {
+                id: list.id,
+                name: list.name,
+                nationality: {
+                  ccode: list.ccode,
+                  cname: list.cname,
+                },
+                picture: `https://images.fotmob.com/image_resources/playerimages/${list.id}.png`,
+              };
+            }),
+          };
+        });
+        return result;
+      };
+
+      const state = status == null ? status : flow();
+      return state;
+    };
+
+    res.json({
+      team: {
+        id: squads.details.id,
+        name: squads.details.name,
+        logo: {
+          normal: `https://images.fotmob.com/image_resources/logo/teamlogo/${squads.details.id}.png`,
+          small: `https://images.fotmob.com/image_resources/logo/teamlogo/${squads.details.id}_xsmall.png`,
+        },
+      },
+      squads: returned(),
+    });
   };
   getSquads();
 };
