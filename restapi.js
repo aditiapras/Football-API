b0d4a0cab7a57852d5b8b87a3c32633e227cd44c
Aditia Prasetian <aditiapras97@gmail.com>
2023-07-31 23:37:12 +0700

    Update restapi.js

--- restapi.js
+++ restapi.js
@@ -13,10 +13,13 @@ const leagueDetails = require("./leagues/leagueDetails.js");
 const leagueTables = require("./leagues/leagueTables.js");
 const leagueTransfers = require("./leagues/leagueTransfers.js");
 const leagueFixtures = require("./leagues/leagueFixtures.js");
-const matchPlayoff = require("./matches/matchesPlayoff.js");
+const matchPlayoff = require("./leagues/leaguePlayoff.js");
 const matchByDate = require("./matches/matchesByDate.js");
 const liveMatches = require("./matches/liveMatches.js");
 const matchDetails = require("./matches/matchDetail.js");
+const matchEvents = require("./matches/events.js");
+const matchLineups = require("./matches/lineups.js");
+const matchStats = require("./matches/stats.js");
 const location = require("./mylocation/location.js");
 
 require("dotenv").config();
@@ -25,6 +28,19 @@ app.use(cors());
 
 let publicPath = path.join(__dirname, "public");
 let docPath = path.join(__dirname, "public");
+
+const isAuth = (req, res, next) => {
+  const auth = req.header("X-API-KEY");
+  const base = req.header("X-Base-URL");
+  if (auth === process.env.API_KEY && base === process.env.BASE_URL) {
+    next();
+  } else {
+    res.status(401);
+    res.json({
+      error: "Access forbidden, you are not authorized to access this API!",
+    });
+  }
+};
 
 // API HOMEPAGE
 app.get("/", (req, res) => {
@@ -54,7 +70,10 @@ app.get("/matches/playoff", matchPlayoff);
 app.get("/matches", matchByDate);
 app.get("/matches/live", liveMatches);
 app.get("/matches/details", matchDetails);
+app.get("/matches/events", matchEvents);
+app.get("/matches/stats", matchStats);
+app.get("/matches/lineups", matchLineups);
 
-app.get("/mylocation", location);
+app.get("/mylocation", isAuth, location);
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
