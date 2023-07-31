c9cc05fdf159297bc35f3c2b37d0983a9938a149
aditiaprasdev <aditiapras97@gmail.com>
2023-07-31 15:40:24 +0700

    Create location.js

---
+++ mylocation/location.js
@@ -0,0 +1,21 @@
+const moment = require("moment-timezone");
+module.exports = (req, res) => {
+  const getIp = async () => {
+    const timezone = moment.tz.guess();
+    const ip = await fetch(`https://api64.ipify.org?format=json`).then((r) =>
+      r.json()
+    );
+    const geo = await fetch(
+      `http://www.geoplugin.net/json.gp?ip=${ip.ip}`
+    ).then((r) => r.json());
+
+    res.json({
+      IP: ip.ip,
+      timezone,
+      city: geo.geoplugin_city,
+      region: geo.geoplugin_region,
+      ccode: geo.geoplugin_countryCode,
+    });
+  };
+  getIp();
+};
