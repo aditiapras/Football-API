d08b8739925336bb6c7356d922359f3d40b2a4ff
Aditia Prasetian <aditiapras97@gmail.com>
2023-07-31 19:12:14 +0700

    Update leagueFixtures.js

--- leagues/leagueFixtures.js
+++ leagues/leagueFixtures.js
@@ -3,42 +3,51 @@ module.exports = (req, res) => {
   const season = req.query.season;
 
   const getMatches = async () => {
-    const matches = await fetch(
-      `https://www.fotmob.com/api/leagues?id=${id}&season=${season}`
-    ).then((r) => r.json());
+    if (!id) {
+      return res
+        .status(400)
+        .json({ result: null, error: "Required League ID" });
+    } else {
+      try {
+        const matches = await fetch(
+          `https://www.fotmob.com/api/leagues?id=${id}&season=${season}`
+        ).then((r) => r.json());
 
-    const allMatches = matches.matches.allMatches.map((match) => {
-      return {
-        round: match.round,
-        roundName: match.roundName,
-        id: match.id,
-        home: {
-          name: match.home.name,
-          shortName: match.home.shortName,
-          id: match.home.id,
-          logo: {
-            normal: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}.png`,
-            small: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}_xsmall.png`,
-          },
-        },
-        away: {
-          name: match.away.name,
-          shortName: match.away.shortName,
-          id: match.away.id,
-          logo: {
-            normal: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}.png`,
-            small: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}_xsmall.png`,
-          },
-        },
-        status: match.status,
-      };
-    });
+        const allMatches = matches.matches.allMatches.map((match) => {
+          return {
+            round: match.round,
+            roundName: match.roundName,
+            id: match.id,
+            home: {
+              name: match.home.name,
+              shortName: match.home.shortName,
+              id: match.home.id,
+              logo: {
+                normal: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}.png`,
+                small: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}_xsmall.png`,
+              },
+            },
+            away: {
+              name: match.away.name,
+              shortName: match.away.shortName,
+              id: match.away.id,
+              logo: {
+                normal: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}.png`,
+                small: `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}_xsmall.png`,
+              },
+            },
+            status: match.status,
+          };
+        });
 
-    res.json({ matches: { allMatches: allMatches } });
+        res.json({ matches: { allMatches: allMatches } });
+      } catch {
+        return res
+          .status(400)
+          .json({ result: null, error: "League ID is invalid" });
+      }
+    }
   };
-  if (!id) {
-    res.status(400).json({ result: null, error: "Required League ID" });
-  } else {
-    getMatches();
-  }
+
+  getMatches();
 };
