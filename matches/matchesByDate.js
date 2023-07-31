d60f58bef2fded973576a0da58a18e5275731e75
aditiaprasdev <aditiapras97@gmail.com>
2023-07-31 15:40:22 +0700

    Update matchesByDate.js

--- matches/matchesByDate.js
+++ matches/matchesByDate.js
@@ -1,11 +1,21 @@
+const moment = require("moment-timezone");
+
 module.exports = (req, res) => {
   const date = req.query.date;
+  const timezone = req.query.timezone;
+  const ccode = req.query.ccode;
+
   const getMatches = async () => {
     const matches = await fetch(
-      `https://www.fotmob.com/api/matches?date=${date}`
+      `https://www.fotmob.com/api/matches?date=${date}&ccode3=${ccode}&timezone=${timezone}`
     ).then((r) => r.json());
 
-    res.json({ leagues: matches.leagues });
+    res.json({
+      date: date,
+      timezone: timezone,
+      ccode: ccode,
+      leagues: matches.leagues,
+    });
   };
   getMatches();
 };
