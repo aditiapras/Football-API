fe783369099de2366bbe3991ea3526ff6ed9ad55
Aditia Prasetian <aditiapras97@gmail.com>
2023-07-31 23:37:03 +0700

    Update matchesByDate.js

--- matches/matchesByDate.js
+++ matches/matchesByDate.js
@@ -4,16 +4,18 @@ module.exports = (req, res) => {
   const ccode = req.query.ccode;
 
   const getMatches = async () => {
-    const matches = await fetch(
-      `https://www.fotmob.com/api/matches?date=${date}&ccode3=${ccode}&timezone=${timezone}`
-    ).then((r) => r.json());
+    if (!date) {
+      res.status(400).json({ result: null, message: "Required Date" });
+    } else {
+      const matches = await fetch(
+        `https://www.fotmob.com/api/matches?date=${date}&ccode3=${ccode}&timezone=${timezone}`
+      ).then((r) => r.json());
 
-    res.json({
-      date: date,
-      timezone: timezone,
-      ccode: ccode,
-      leagues: matches.leagues,
-    });
+      res.json({
+        query: { date: date, ccode: ccode, timezone: timezone },
+        leagues: matches.leagues,
+      });
+    }
   };
   getMatches();
 };
