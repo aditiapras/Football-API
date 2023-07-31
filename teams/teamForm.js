663ac9c96b6ed3ffdd2615c7845fd13bcc9bf22e
Aditia Prasetian <aditiapras97@gmail.com>
2023-07-31 23:37:14 +0700

    Update teamForm.js

--- teams/teamForm.js
+++ teams/teamForm.js
@@ -1,37 +1,43 @@
 module.exports = (req, res) => {
   const id = req.query.id;
   const getForms = async () => {
-    const getForm = await fetch(
-      `https://www.fotmob.com/api/teams?id=${id}`
-    ).then((r) => r.json());
+    if (!id) {
+      res.status(400).json({ result: null, error: "Required Team ID" });
+    } else {
+      try {
+        const getForm = await fetch(
+          `https://www.fotmob.com/api/teams?id=${id}`
+        ).then((r) => r.json());
 
-    const forms = getForm.overview.teamForm.map((form) => {
-      return {
-        result: form.result,
-        resultString: form.resultString,
-        home: {
-          name: form.tooltipText.homeTeam,
-          id: form.tooltipText.homeTeamId,
-          logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${form.tooltipText.homeTeamId}_xsmall.png`,
-          score: form.tooltipText.homeScore,
-        },
-        away: {
-          name: form.tooltipText.awayTeam,
-          id: form.tooltipText.awayTeamId,
-          logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${form.tooltipText.awayTeamId}_xsmall.png`,
-          score: form.tooltipText.awayScore,
-        },
-        score: form.score,
-        date: form.date,
-      };
-    });
+        const forms = getForm.overview.teamForm.map((form) => {
+          return {
+            result: form.result,
+            resultString: form.resultString,
+            home: {
+              name: form.tooltipText.homeTeam,
+              id: form.tooltipText.homeTeamId,
+              logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${form.tooltipText.homeTeamId}_xsmall.png`,
+              score: form.tooltipText.homeScore,
+            },
+            away: {
+              name: form.tooltipText.awayTeam,
+              id: form.tooltipText.awayTeamId,
+              logo: `https://images.fotmob.com/image_resources/logo/teamlogo/${form.tooltipText.awayTeamId}_xsmall.png`,
+              score: form.tooltipText.awayScore,
+            },
+            score: form.score,
+            date: form.date,
+          };
+        });
 
-    res.json({ forms });
+        res.json({ forms });
+      } catch {
+        return res
+          .status(400)
+          .json({ result: null, error: "Team ID is invalid!" });
+      }
+    }
   };
 
-  if (!id) {
-    res.status(400).json({ result: null, error: "Required Team ID" });
-  } else {
-    getForms();
-  }
+  getForms();
 };
