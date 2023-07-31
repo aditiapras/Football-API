ea0091b44b145f345310e5d0005fc20068d0523a
Aditia Prasetian <aditiapras97@gmail.com>
2023-07-31 19:12:12 +0700

    Update leagueDetails.js

--- leagues/leagueDetails.js
+++ leagues/leagueDetails.js
@@ -3,74 +3,82 @@ module.exports = (req, res) => {
   const season = req.query.season;
 
   const getLeagues = async () => {
-    const league = await fetch(
-      `https://www.fotmob.com/api/leagues?id=${id}&season=${season}`
-    ).then((r) => r.json());
-    const leagues = await league;
-    const allAvailableSeasons = leagues.allAvailableSeasons;
+    if (!id) {
+      res.status(400).json({
+        result: null,
+        message: "Required League ID. League ID cannot be empty",
+      });
+    } else {
+      try {
+        const league = await fetch(
+          `https://www.fotmob.com/api/leagues?id=${id}&season=${season}`
+        ).then((r) => r.json());
+        const leagues = await league;
 
-    const details = {
-      id: leagues.details.id,
-      type: leagues.details.type,
-      name: leagues.details.name,
-      selectedSeason: leagues.details.selectedSeason,
-      latestSeason: leagues.details.latestSeason,
-      shortName: leagues.details.shortName,
-      country: leagues.details.country,
-      logo: {
-        light: `https://images.fotmob.com/image_resources/logo/leaguelogo/${leagues.details.id}.png`,
-        dark: `https://images.fotmob.com/image_resources/logo/leaguelogo/dark/${leagues.details.id}.png`,
-      },
-    };
+        const allAvailableSeasons = leagues.allAvailableSeasons;
 
-    const match = leagues.matches;
-    const matches = {};
-    matches.firstUnplayedMatch = match.firstUnplayedMatch;
-    matches.allMatches = match.allMatches.map((match) => {
-      const home = {
-        name: match.home.name,
-        shortName: match.home.shortName,
-        id: match.home.id,
-        logo: {
-          normal:
-            (match.home.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}.png`),
-          small:
-            (match.home.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}_xsmall.png`),
-        },
-      };
+        const details = {
+          id: leagues.details.id,
+          type: leagues.details.type,
+          name: leagues.details.name,
+          selectedSeason: leagues.details.selectedSeason,
+          latestSeason: leagues.details.latestSeason,
+          shortName: leagues.details.shortName,
+          country: leagues.details.country,
+          logo: {
+            light: `https://images.fotmob.com/image_resources/logo/leaguelogo/${leagues.details.id}.png`,
+            dark: `https://images.fotmob.com/image_resources/logo/leaguelogo/dark/${leagues.details.id}.png`,
+          },
+        };
 
-      const away = {
-        name: match.away.name,
-        shortName: match.away.shortName,
-        id: match.away.id,
-        logo: {
-          normal:
-            (match.away.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}.png`),
-          small:
-            (match.away.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}_xsmall.png`),
-        },
-      };
+        const match = leagues.matches;
+        const matches = {};
+        matches.firstUnplayedMatch = match.firstUnplayedMatch;
 
-      return {
-        query: { id: id, season: season },
-        round: match.round,
-        roundName: match.roundName,
-        id: match.id,
-        home: home,
-        away: away,
-        status: match.status,
-      };
-    });
+        matches.allMatches = match.allMatches.map((match) => {
+          const home = {
+            name: match.home.name,
+            shortName: match.home.shortName,
+            id: match.home.id,
+            logo: {
+              normal:
+                (match.home.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}.png`),
+              small:
+                (match.home.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}_xsmall.png`),
+            },
+          };
 
-    res.json({ details, allAvailableSeasons });
+          const away = {
+            name: match.away.name,
+            shortName: match.away.shortName,
+            id: match.away.id,
+            logo: {
+              normal:
+                (match.away.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}.png`),
+              small:
+                (match.away.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}_xsmall.png`),
+            },
+          };
+
+          return {
+            query: { id: id, season: season },
+            round: match.round,
+            roundName: match.roundName,
+            id: match.id,
+            home: home,
+            away: away,
+            status: match.status,
+          };
+        });
+
+        res.json({ details, allAvailableSeasons });
+      } catch {
+        return res
+          .status(400)
+          .json({ result: null, message: "League ID is invalid" });
+      }
+    }
   };
 
-  if (!id) {
-    res.status(400).json({
-      result: null,
-      error: "Required League ID, League ID cannot be empty!",
-    });
-  } else {
-    getLeagues();
-  }
+  getLeagues();
 };
