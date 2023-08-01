8071e0a8dc4ee0d621686210a3d10f55d3da3e0a
aditiaprasdev <aditiapras97@gmail.com>
2023-08-01 15:17:22 +0700

    Update stats.js

--- matches/stats.js
+++ matches/stats.js
@@ -1,15 +1,17 @@
 module.exports = (req, res) => {
   const matchId = req.query.matchId;
-  const getEvents = async () => {
+  const getStats = async () => {
     if (!matchId) {
       res.status(400).json({ result: null, message: "Required Match ID!" });
     } else {
       try {
-        const events = await fetch(
+        const stats = await fetch(
           `https://www.fotmob.com/api/matchDetails?matchId=${matchId}`
         ).then((r) => r.json());
 
-        res.json({ events });
+        const stat = stats.content.stats;
+
+        res.json({ stats: stat });
       } catch {
         return res
           .status(400)
@@ -17,5 +19,5 @@ module.exports = (req, res) => {
       }
     }
   };
-  getEvents();
+  getStats();
 };
