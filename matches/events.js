c38373d440882790b8895b279a5992b6f22f6a84
aditiaprasdev <aditiapras97@gmail.com>
2023-08-01 15:17:13 +0700

    Update events.js

--- matches/events.js
+++ matches/events.js
@@ -8,18 +8,38 @@ module.exports = (req, res) => {
         const events = await fetch(
           `https://www.fotmob.com/api/matchDetails?matchId=${matchId}`
         ).then((r) => r.json());
-        const event = events.content.matchFacts.events.events;
+        const event = events.content.matchFacts.events;
 
-        const eve = event.map((result) => {
+        const eve = event.events.map((result) => {
           const removed = result;
           delete removed.profileUrl;
           delete removed.player.profileUrl;
           delete removed.assistProfileUrl;
           delete removed.reactKey;
+
           return removed;
         });
 
-        res.json({ events: eve });
+        const penKick = () => {
+          const shootOut = event.penaltyShootoutEvents.map((pen) => {
+            const removed = pen;
+            delete removed.profileUrl;
+            delete removed.player.profileUrl;
+            delete removed.reactKey;
+            delete removed.shotmapEvent;
+            delete removed.assistStr;
+            delete removed.assistProfileUrl;
+            delete removed.suffix;
+            delete removed.suffixKey;
+
+            return removed;
+          });
+          return shootOut;
+        };
+
+        const penalty = event.penaltyShootoutEvents == null ? null : penKick();
+
+        res.json({ events: eve, penaltyShootoutEvents: penalty });
       } catch {
         return res
           .status(400)
