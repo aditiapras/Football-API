d13f6facf3c2bfaabb62d8d17904b52ed9969603
Aditia Prasetian <aditiapras97@gmail.com>
2023-08-02 22:32:51 +0700

    Update clientLocation.js

--- mylocation/clientLocation.js
+++ mylocation/clientLocation.js
@@ -1,29 +1,24 @@
-const moment = require("moment-timezone");
-const requestIp = require("request-ip");
-
 module.exports = (req, res) => {
-  let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
+  // let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   let ipv = req.ip;
-  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const getIp = async () => {
     const geo = await fetch(`http://www.geoplugin.net/json.gp?ip=${ipv}`).then(
       (r) => r.json()
     );
 
     res.json({
-      ip: ip,
-      ipv,
-      tz,
-      clientIp: geo.geoplugin_request,
-      geoplugin_city: geo.geoplugin_city,
-      geoplugin_region: geo.geoplugin_region,
-      geoplugin_regionCode: geo.geoplugin_regionCode,
-      geoplugin_regionName: geo.geoplugin_regionName,
-      geoplugin_countryCode: geo.geoplugin_countryCode,
-      geoplugin_countryName: geo.geoplugin_countryName,
-      geoplugin_continentCode: geo.geoplugin_continentCode,
-      geoplugin_timezone: geo.geoplugin_timezone,
+      ip: ipv,
+      query: geo.geoplugin_request,
+      city: geo.geoplugin_city,
+      region: geo.geoplugin_region,
+      regionCode: geo.geoplugin_regionCode,
+      regionName: geo.geoplugin_regionName,
+      countryCode: geo.geoplugin_countryCode,
+      countryName: geo.geoplugin_countryName,
+      continentCode: geo.geoplugin_continentCode,
+      timezone: geo.geoplugin_timezone,
     });
   };
   getIp();
+  s;
 };
