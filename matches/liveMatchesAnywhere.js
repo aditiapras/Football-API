21a1054aae78181555bb2edca048d743aef4e2ea
aditiaprasdev <aditiapras97@gmail.com>
2023-08-03 08:54:01 +0700

    Create liveMatchesAnywhere.js

---
+++ matches/liveMatchesAnywhere.js
@@ -0,0 +1,46 @@
+const moment = require("moment-timezone");
+
+module.exports = (req, res) => {
+  const date = moment().format("YYYYMMDD");
+  const currentDate = moment().format("LLLL");
+  const utcTime = moment.utc();
+  const timezone = req.query.timezone;
+  const ccode = req.query.ccode;
+
+  const getLive = async () => {
+    const live = await fetch(
+      `https://www.fotmob.com/api/matches?date=${date}&timezone=${timezone}&ccode3=${ccode}`
+    ).then((r) => r.json());
+
+    const liveMatches = live.leagues.map((league) => {
+      return {
+        id: league.primaryId,
+        name: league.name,
+        logo: {
+          light: `https://images.fotmob.com/image_resources/logo/leaguelogo/${league.primaryId}.png`,
+          dark: `https://images.fotmob.com/image_resources/logo/leaguelogo/dark/${league.primaryId}.png`,
+        },
+        ccode: league.ccode,
+        countryLogo: `https://images.fotmob.com/image_resources/logo/teamlogo/${league.ccode.toLowerCase()}.png`,
+
+        matches: league.matches.filter(
+          (match) =>
+            match.status.finished == false &&
+            match.status.started == true &&
+            match.status.cancelled == false
+        ),
+      };
+    });
+
+    const liveMatch = liveMatches.filter((match) => match.matches != "");
+
+    res.json({
+      datetime: currentDate,
+      utcTime: utcTime,
+      countryCode: ccode,
+      timezone: timezone,
+      liveMatches: liveMatch,
+    });
+  };
+  getLive();
+};
