1763f0095c3d1b5e796245ad74a67a974856b072
aditiaprasdev <aditiapras97@gmail.com>
2023-08-03 08:54:05 +0700

    Update matchesByDate.js

--- matches/matchesByDate.js
+++ matches/matchesByDate.js
@@ -1,18 +1,31 @@
 module.exports = (req, res) => {
   const date = req.query.date;
-  const timezone = req.query.timezone;
-  const ccode = req.query.ccode;
+  // const timezone = req.query.timezone;
+  // const ccode = req.query.ccode;
 
   const getMatches = async () => {
     if (!date) {
       res.status(400).json({ result: null, message: "Required Date" });
     } else {
+      const locations = await fetch(
+        `https://football-api68.p.rapidapi.com/mylocation`,
+        {
+          headers: {
+            "X-RapidAPI-Key": process.env.RAPID_KEY,
+            "X-RapidAPI-Host": process.env.RAPID_BASE,
+          },
+        }
+      ).then((r) => r.json());
+
+      const tz = locations.timezone;
+      const ccode = locations.country_code;
+
       const matches = await fetch(
-        `https://www.fotmob.com/api/matches?date=${date}&ccode3=${ccode}&timezone=${timezone}`
+        `https://www.fotmob.com/api/matches?date=${date}&ccode3=${ccode}&timezone=${tz}`
       ).then((r) => r.json());
 
       res.json({
-        query: { date: date, ccode: ccode, timezone: timezone },
+        query: { date: date, ccode: ccode, timezone: tz },
         leagues: matches.leagues,
       });
     }
