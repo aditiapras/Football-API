45ef8e6f1ed2295f8aabe9d3a202ed46bbfdd587
aditiaprasdev <aditiapras97@gmail.com>
2023-08-07 13:54:52 +0700

    Update leagueDetails.js

--- leagues/leagueDetails.js
+++ leagues/leagueDetails.js
@@ -26,50 +26,10 @@ module.exports = (req, res) => {
           shortName: leagues.details.shortName,
           country: leagues.details.country,
           logo: {
-            light: `https://images.fotmob.com/image_resources/logo/leaguelogo/${leagues.details.id}.png`,
-            dark: `https://images.fotmob.com/image_resources/logo/leaguelogo/dark/${leagues.details.id}.png`,
+            light: `https://media-1.api.aditiapras.dev/image_resources/logo/leaguelogo/light/${leagues.details.id}.png`,
+            dark: `https://media-1.api.aditiapras.dev/image_resources/logo/leaguelogo/dark/${leagues.details.id}.png`,
           },
         };
-
-        const match = leagues.matches;
-        const matches = {};
-        matches.firstUnplayedMatch = match.firstUnplayedMatch;
-
-        matches.allMatches = match.allMatches.map((match) => {
-          const home = {
-            name: match.home.name,
-            shortName: match.home.shortName,
-            id: match.home.id,
-            logo: {
-              normal:
-                (match.home.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}.png`),
-              small:
-                (match.home.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.home.id}_xsmall.png`),
-            },
-          };
-
-          const away = {
-            name: match.away.name,
-            shortName: match.away.shortName,
-            id: match.away.id,
-            logo: {
-              normal:
-                (match.away.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}.png`),
-              small:
-                (match.away.logo = `https://images.fotmob.com/image_resources/logo/teamlogo/${match.away.id}_xsmall.png`),
-            },
-          };
-
-          return {
-            query: { id: id, season: season },
-            round: match.round,
-            roundName: match.roundName,
-            id: match.id,
-            home: home,
-            away: away,
-            status: match.status,
-          };
-        });
 
         res.json({ details, allAvailableSeasons });
       } catch {
